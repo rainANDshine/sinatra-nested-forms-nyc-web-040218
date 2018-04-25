2c8133596e8cfaaad273fa2d3221594247208619
github username <github email address>
2018-04-24 20:07:13 -0400

    started

--- app.rb
+++ app.rb
@@ -2,8 +2,19 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+      erb :new
+    end
 
-    # code other routes/actions here
+    post '/pirates' do
+      @student = Student.new(params[:student])
 
+      params[:student][:courses].each do |details|
+        Course.new(details)
+      end
+
+      @courses = Course.all
+      erb :show
+    end
   end
 end
